d4a7239ff1d447f66d62fa33008e3490d49e5d12
Dmitry <kyrnickijdima@gmail.com>
2017-05-31 12:48:27 +0300

    Update Engine.java

--- java/Engine/Engine.java
+++ java/Engine/Engine.java
@@ -2,37 +2,21 @@ package Engine;
 
 import Frames.FirstFrame;
 import Frames.MainFrame;
-import Implements.WorkUA;
-import ParseHtml.ParserWorkUA;
-import Scanner.ScanWebSite;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 
 /**
  * Created by dmitry on 17.05.17.
  */
 public class Engine {
 
-    public static WorkUA workUA;
     public static MainFrame mainFrame;
-
+    public CreateOutList list = new CreateOutList();
 
     public void begin() {
-        boolean[] sites = FirstFrame.sites;
-        if (sites[0]){
-           workUA = new WorkUA(FirstFrame.keyWords, FirstFrame.city);
-        }
-
-
-        ScanWebSite s = new ScanWebSite();
-        String text = null;
-        try {
-            text = s.getHtmlCode(workUA);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        ParserWorkUA parser = new ParserWorkUA();
-        mainFrame = new MainFrame(parser.parse(text));
+        mainFrame = new MainFrame(list.create());
         mainFrame.init();
+
     }
 }
